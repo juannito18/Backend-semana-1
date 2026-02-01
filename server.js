b0fda4e14ca6cc82f3fb1abbd60b9796a7d17019
Juan <juanfilito@gmail.com>
2026-02-01 10:31:18 -0300

    feat: agregar ruta raiz

--- server.js
+++ server.js
@@ -109,6 +109,10 @@ app.use((err, req, res, next) => {
 });
 
 // Iniciar servidor
+app.get("/", (req, res) => {
+  res.send("API de productos funcionando 🚀");
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
