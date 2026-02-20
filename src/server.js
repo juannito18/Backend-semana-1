c67004dbb44ff7f28654fd8f5bd20d817bf3491f
Juan <juanfilito@gmail.com>
2026-02-20 09:59:15 -0300

    CRUD users added

--- src/server.js
+++ src/server.js
@@ -12,6 +12,55 @@ pool.query("SELECT NOW()", (err, res) => {
     console.log("DB conectada:", res.rows);
   }
 });
+app.get("/users", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.post("/users", async (req, res) => {
+  const { name, email } = req.body;
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *",
+      [name, email]
+    );
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.put("/users/:id",async(req, res)=>{
+  const { id }= req.params;
+  const {name, email} = req.body;
+
+try {
+  const result = await pool.query(
+    "UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *",
+      [name, email, id]
+  );
+  res.json(result.rows[0]);
+} catch (error) {
+   res.status(500).json({ error: error.message });
+}
+
+
+
+})
+app.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await pool.query("DELETE FROM users WHERE id=$1", [id]);
+    res.json({ message: "Usuario eliminado" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
